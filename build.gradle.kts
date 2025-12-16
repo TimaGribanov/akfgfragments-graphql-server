872f70e4fafe067dfe97b3707453361325dcaf01
TimaGribanov <timofei.gribanov@ericsson.com>
2025-12-16 15:54:03 +0100

    workflow: graalvm buildArgs

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ graalvmNative {
         named("main") {
             verbose.set(true)
 
-            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin,ch.qos.logback,org.slf4j,kotlinx")
+            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin,ch.qos.logback,org.slf4j,kotlinx,org.xml.sax.helpers.LocatorImpl")
             buildArgs.add("-H:ReflectionConfigurationFiles=../../../src/main/resources/META-INF/native-compile/reflection.json")
 
             // options to configure the main binary
