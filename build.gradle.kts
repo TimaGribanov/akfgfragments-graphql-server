c54c1356572d9895828568f8b1fc435ce2344e41
TimaGribanov <timofei.gribanov@ericsson.com>
2026-01-12 12:13:19 +0100

    making build process suitable for the server; adapting the workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.UsesKotlinJavaToolchain
-
 description = "akfgfragments.com - back-end custom data GraphQL server"
 
 plugins {
@@ -12,7 +10,15 @@ plugins {
 }
 
 group = "com.akfgfragments"
-version = "0.1.6"
+version = "0.1.9"
+
+application {
+    mainClass.set("com.akfgfragments.ApplicationKt")
+}
+
+kotlin {
+    jvmToolchain(25)
+}
 
 repositories {
     mavenCentral()
@@ -42,32 +48,37 @@ dependencies {
 //    implementation(kotlin("stdlib-jdk8"))
 }
 
-project.tasks.withType<UsesKotlinJavaToolchain>().configureEach {
-    kotlinJavaToolchain.toolchain.use(project.extensions.getByType<JavaToolchainService>().launcherFor {
-        languageVersion.set(JavaLanguageVersion.of(25))
-    })
-}
-
-//kotlin {
-//    jvmToolchain(22)
-//}
-
-application {
-    mainClass.set("io.ktor.server.cio.EngineMain")
+ktor {
+    fatJar {
+        archiveFileName.set("akfgfragments-data-graphql-server-fatJar-${version}.jar")
+    }
 }
 
 graalvmNative {
     toolchainDetection.set(false)
+
+    agent {
+        enabled.set(true)
+        metadataCopy {
+            inputTaskNames.add("run")
+            outputDirectories.add("src/main/resources/META-INF/native-image/org.akfgfragments")
+            mergeWithExisting.set(true)
+        }
+    }
+
     binaries {
         named("main") {
             verbose.set(true)
 
-            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin,ch.qos.logback,org.slf4j,kotlinx,org.xml.sax.helpers")
-            buildArgs.add("-H:ReflectionConfigurationFiles=../../../src/main/resources/META-INF/native-compile/reflection.json")
+            buildArgs.add("--initialize-at-build-time=io.ktor,kotlin")
+            buildArgs.add("--initialize-at-build-time=ch.qos.logback")
+            buildArgs.add("--initialize-at-build-time=org.slf4j")
+            buildArgs.add("--initialize-at-build-time=kotlinx")
+            buildArgs.add("--initialize-at-build-time=org.xml.sax.helpers")
 
-            // options to configure the main binary
             imageName.set("akfgfragments-data-graphql-server-${version}")
         }
+
         metadataRepository {
             enabled.set(true)
         }
--- src/main/kotlin/com/akfgfragments/Application.kt
+++ src/main/kotlin/com/akfgfragments/Application.kt
@@ -1,12 +1,14 @@
 package com.akfgfragments
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.log
+import io.ktor.server.application.port
 
 fun main(args: Array<String>): Unit = io.ktor.server.cio.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
-//    log.info("The app is running on port: {}", port)
+    log.info("The app is running on port: {}", environment.config.port)
 
     configureDatabases(environment.config)
     configureRouting()
